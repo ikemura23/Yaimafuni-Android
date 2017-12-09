7e981cc518de3db31c5d43e0e3e860d69a9429c7
ikemura <8417910+banbara23@users.noreply.github.com>
2017-12-10 00:39:52 +0900

    天気の見た目完成

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherActivity.java
@@ -1,11 +1,8 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.front.weather;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R;
 
@@ -14,17 +11,17 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.weather_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherFragment.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherFragment.java
@@ -19,6 +19,6 @@ public class WeatherFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        return inflater.inflate(R.layout.weather_fragment, container, false);
     }
 }
