527e6679e8254a1b131a9660968a5aa56bfd71e1
ikemura <8417910+banbara23@users.noreply.github.com>
2017-12-17 11:12:16 +0900

    top画面の天気に>アイコンを追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/weather/WeatherActivity.java
@@ -14,14 +14,5 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.weather_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 }
