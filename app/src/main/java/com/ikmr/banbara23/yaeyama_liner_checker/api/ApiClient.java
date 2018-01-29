cac9719a43e1e2e8ad9933532cbc46bd1941d77b
ikemura <k_ikemura@yumemi.co.jp>
2018-01-29 14:24:46 +0900

    運行詳細もFlowable.zipにする

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/api/ApiClient.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/api/ApiClient.java
@@ -1,12 +1,7 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.api;
 
-import android.util.Log;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Company;
 import com.ikmr.banbara23.yaeyama_liner_checker.model.CompanyStatus;
 import com.ikmr.banbara23.yaeyama_liner_checker.model.DetailLinerInfo;
@@ -18,16 +13,9 @@ import com.ikmr.banbara23.yaeyama_liner_checker.model.weather.WeatherInfo;
 
 import durdinapps.rxfirebase2.RxFirebaseDatabase;
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
-import io.reactivex.Single;
-import io.reactivex.SingleEmitter;
-import io.reactivex.SingleOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Function3;
 import io.reactivex.schedulers.Schedulers;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * APIクライアント
@@ -77,32 +65,10 @@ public class ApiClient {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
     }
-//        ref.keepSynced(false);
-//        return Single.create(new SingleOnSubscribe<TopCompanyInfo>() {
-//            @Override
-//            public void subscribe(@NonNull final SingleEmitter<TopCompanyInfo> e) throws Exception {
-//                // APIリクエスト
-//                ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(DataSnapshot dataSnapshot) {
-//                        TopCompanyInfo topCompanyInfo = dataSnapshot.getValue(TopCompanyInfo.class);
-//                        Log.d(TAG, "Value is: " + topCompanyInfo.toString());
-//                        e.onSuccess(topCompanyInfo);
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(DatabaseError error) {
-//                        Log.w(TAG, "Failed to read value.", error.toException());
-//                        e.onError(error.toException());
-//                    }
-//                });
-//            }
-//        });
-//}
 
-    public static Observable<StatusDetailRoot> getDetailInfo(Company company, String portCode) {
+    public static Flowable<StatusDetailRoot> getDetailInfo(Company company, String portCode) {
 
-        return Observable.zip(
+        return Flowable.zip(
                 // ステータスのみ
                 getStatusDetail(company, portCode),
                 // 運行関連（走行時間、金額など）
@@ -125,33 +91,13 @@ public class ApiClient {
      *
      * @return
      */
-    public static Observable<PortStatus> getStatusDetail(Company company, String portCode) {
+    public static Flowable<PortStatus> getStatusDetail(Company company, String portCode) {
         final String path = company.getCode() + "/" + portCode;
         final DatabaseReference ref = getRef(path);
         ref.keepSynced(false);
-        return Single.create(new SingleOnSubscribe<PortStatus>() {
-            @Override
-            public void subscribe(@NonNull final SingleEmitter<PortStatus> e) throws Exception {
-                // APIリクエスト
-                ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        PortStatus data = dataSnapshot.getValue(PortStatus.class);
-                        if (data == null) {
-                            e.onError(new Exception(path + " api response is Null"));
-                            return;
-                        }
-                        e.onSuccess(data);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        Log.w(TAG, "Failed to read value.", error.toException());
-                        e.onError(error.toException());
-                    }
-                });
-            }
-        }).toObservable();
+        return RxFirebaseDatabase.observeValueEvent(ref, PortStatus.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 
     /**
@@ -159,34 +105,13 @@ public class ApiClient {
      *
      * @return
      */
-    public static Observable<DetailLinerInfo> getDetailLinerInfo(Company company, String portCode) {
+    public static Flowable<DetailLinerInfo> getDetailLinerInfo(Company company, String portCode) {
         final String path = company.getCode() + "_liner_info/" + portCode;
         final DatabaseReference ref = getRef(path);
         ref.keepSynced(false);
-        return Single.create(new SingleOnSubscribe<DetailLinerInfo>() {
-            @Override
-            public void subscribe(@NonNull final SingleEmitter<DetailLinerInfo> e) throws Exception {
-                // データの 1 回読み取り
-                ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        DetailLinerInfo data = dataSnapshot.getValue(DetailLinerInfo.class);
-                        if (data == null) {
-                            e.onError(new Exception(path + " api response is Null"));
-                            return;
-                        }
-                        Log.d(TAG, "Value is: " + data.toString());
-                        e.onSuccess(data);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        Log.w(TAG, "Failed to read value.", error.toException());
-                        e.onError(error.toException());
-                    }
-                });
-            }
-        }).toObservable();
+        return RxFirebaseDatabase.observeValueEvent(ref, DetailLinerInfo.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 
     /**
@@ -194,32 +119,13 @@ public class ApiClient {
      *
      * @return
      */
-    public static Observable<TimeTable> getTimeTable(Company company, String portCode) {
+    public static Flowable<TimeTable> getTimeTable(Company company, String portCode) {
         final String path = company.getCode() + "_timeTable/" + portCode;
         final DatabaseReference ref = getRef(path);
         ref.keepSynced(false);
-        return Single.create(new SingleOnSubscribe<TimeTable>() {
-            @Override
-            public void subscribe(@NonNull final SingleEmitter<TimeTable> e) throws Exception {
-                // データの 1 回読み取り
-                ref.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        TimeTable data = dataSnapshot.getValue(TimeTable.class);
-                        if (data == null) {
-                            data = new TimeTable();
-                        }
-                        e.onSuccess(data);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        Log.w(TAG, "Failed to read value.", error.toException());
-                        e.onError(error.toException());
-                    }
-                });
-            }
-        }).toObservable();
+        return RxFirebaseDatabase.observeValueEvent(ref, TimeTable.class)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 
     /**
