c25f4d0b8a6c35d34c008a8d5f93de2bd96b4013
ikemura <8417910+banbara23@users.noreply.github.com>
2018-03-29 11:48:23 +0900

    港別のapiレスポンスをDataBingingで表示できた、レイアウトはメチャクチャだけど

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/top/TopPort.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/top/TopPort.kt
@@ -4,6 +4,7 @@ data class TopPort(
         var hateruma: Ports = Ports(),
         var hatoma: Ports = Ports(),
         var kohama: Ports = Ports(),
+        var taketomi: Ports = Ports(),
         var kuroshima: Ports = Ports(),
         var oohara: Ports = Ports(),
         var uehara: Ports = Ports()
