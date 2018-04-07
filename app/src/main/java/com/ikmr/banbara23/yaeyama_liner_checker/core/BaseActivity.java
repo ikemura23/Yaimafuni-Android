918b117e6725ba17d48c052991a623fdfc57bcfd
ikemura <8417910+banbara23@users.noreply.github.com>
2018-04-07 14:36:39 +0900

    OS4.4でvectorDrawableが表示できないバグ修正

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/core/BaseActivity.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/core/BaseActivity.java
@@ -2,7 +2,10 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.core;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 
 public class BaseActivity extends AppCompatActivity {
-   
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 }
