1dd31fffd1c60f987a8206e783d31f6157d98dfd
ikemura <k_ikemura@yumemi.co.jp>
2018-04-07 12:58:04 +0900

    トップ港別のステータスに色をつけた

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.java
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.java
@@ -8,6 +8,9 @@ import com.ikmr.banbara23.yaeyama_liner_checker.core.Base;
 public class StatusHelper {
 
     public static int getStatusColor(String code) {
+        if (code == null) {
+            return ContextCompat.getColor(Base.getContext(), R.color.status_cation);
+        }
         switch (code) {
             case "normal":
                 return ContextCompat.getColor(Base.getContext(), R.color.status_normal);
