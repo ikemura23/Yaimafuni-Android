1ae5d6addf9a94c58af6104c36d0f984538aa8dc
banbara23 <8417910+banbara23@users.noreply.github.com>
2019-12-13 16:30:02 +0900

    リネーム TyphoonDetail -> TyphoonList

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/typhoon/TyphoonListFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/typhoon/TyphoonListFragment.kt
@@ -9,7 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.api.ApiClient
-import com.ikmr.banbara23.yaeyama_liner_checker.databinding.TyphoonDetailFragmentBinding
+import com.ikmr.banbara23.yaeyama_liner_checker.databinding.TyphoonListFragmentBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Typhoon
 import com.ikmr.banbara23.yaeyama_liner_checker.utils.CustomTabUtil
 import io.reactivex.disposables.CompositeDisposable
@@ -20,13 +20,13 @@ class TyphoonDetailFragment : Fragment(), OnTyphoonDetailFragmentInteractionList
     private val apiClient = ApiClient()
     private val disposable = CompositeDisposable()
 
-    private lateinit var binding: TyphoonDetailFragmentBinding
+    private lateinit var binding: TyphoonListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.typhoon_detail_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.typhoon_list_fragment, container, false)
         binding.includeTitleBar.titleBar.setNavigationOnClickListener { activity?.finish() }
         binding.list.adapter = TyphoonRecyclerViewAdapter(listOf(), this)
         return binding.root
@@ -43,21 +43,21 @@ class TyphoonDetailFragment : Fragment(), OnTyphoonDetailFragmentInteractionList
     private fun fetchTyphoon() {
         disposable.add(
             apiClient
-                    .typhoon
-                    .subscribeWith(
-                        object : ResourceSubscriber<List<Typhoon>>() {
-                            override fun onNext(typhoonList: List<Typhoon>) {
-                                Log.d("fetchTyphoon", typhoonList.toString())
-                                bindTyphoon(typhoonList)
-                            }
+                .typhoon
+                .subscribeWith(
+                    object : ResourceSubscriber<List<Typhoon>>() {
+                        override fun onNext(typhoonList: List<Typhoon>) {
+                            Log.d("fetchTyphoon", typhoonList.toString())
+                            bindTyphoon(typhoonList)
+                        }
 
-                            override fun onError(t: Throwable) {
-                                Log.d("fetchTyphoon", t.toString())
-                            }
+                        override fun onError(t: Throwable) {
+                            Log.d("fetchTyphoon", t.toString())
+                        }
 
-                            override fun onComplete() {
-                            }
-                        })
+                        override fun onComplete() {
+                        }
+                    })
         )
     }
 
