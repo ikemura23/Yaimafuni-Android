17edb95b5fe239378167935210287a86521148b5
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-01-25 11:03:07 +0900

    台風data classにコメント追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/Typhoon.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/Typhoon.kt
@@ -8,12 +8,20 @@ data class TyphoonRoot(
  * 台風のモデル
  */
 data class Typhoon(
+    /** 名前 */
     val name: String = "",
+    /** 更新日 */
     val dateTime: String = "",
+    /** 画像 */
     val img: String = "",
+    /** 大きさ */
     val scale: String = "",
+    /** 強さ */
     val intensity: String = "",
+    /** 気圧 */
     val pressure: String = "",
+    /** 存在地域 */
     val area: String = "",
+    /** 中心の最大風速 */
     val maxWindSpeedNearCenter: String = ""
 )
