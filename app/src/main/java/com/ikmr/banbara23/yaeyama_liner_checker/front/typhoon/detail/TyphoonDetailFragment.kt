73ff8595a78bdcfb9dcf056cdd334c938e08ebc8
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-01-25 12:01:28 +0900

    台風詳細にargsを表示できた

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/typhoon/detail/TyphoonDetailFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/typhoon/detail/TyphoonDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.front.typhoon.detail
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,18 +9,33 @@ import android.view.ViewGroup
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.common.Constants
+import com.ikmr.banbara23.yaeyama_liner_checker.databinding.FragmentTyphoonDetailBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Typhoon
+import com.squareup.picasso.Picasso
 
+/**
+ * 台風詳細 Fragment
+ */
 class TyphoonDetailFragment : Fragment() {
-    
-    val typhoon: Typhoon
-        get() = arguments?.getParcelable(Constants.BUNDLE_KEY_DETAIL) as Typhoon
+    private lateinit var binding: FragmentTyphoonDetailBinding
+    private val typhoon: Typhoon
+        get() = arguments!!.getParcelable(Constants.BUNDLE_KEY_DETAIL) as Typhoon
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_typhoon_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_typhoon_detail, container, false)
+        binding.typhoon = typhoon
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // 戻る設定
+        binding.includeTitleBar.titleBar.setNavigationOnClickListener { requireActivity().finish() }
+        // 画像読み込み
+        Picasso.get().load(typhoon.img).into(binding.image)
     }
 
     companion object {
