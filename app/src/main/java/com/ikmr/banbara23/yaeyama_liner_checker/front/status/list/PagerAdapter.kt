0b2fca9ba40b7a254654997b9179c349946606fd
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-02-15 17:57:59 +0900

    クラスがフルパッケージ名になってたので修正

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/status/list/PagerAdapter.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/status/list/PagerAdapter.kt
@@ -3,16 +3,14 @@ package com.ikmr.banbara23.yaeyama_liner_checker.front.status.list
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Company
 
 /**
  * PagerAdapter
  */
-class PagerAdapter(fm: androidx.fragment.app.FragmentManager, internal var mNumOfTabs: Int) :
-    androidx.fragment.app.FragmentStatePagerAdapter(fm) {
+class PagerAdapter(fm: FragmentManager, internal var mNumOfTabs: Int) : FragmentStatePagerAdapter(fm) {
 
-    override fun getItem(position: Int): androidx.fragment.app.Fragment {
+    override fun getItem(position: Int): Fragment {
         val company: Company?
         when (position) {
             0 -> company = Company.ANEI
