1b287eb06167056538e94de952867c0859e97355
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-02-15 17:54:53 +0900

    remove package

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/core/BaseListFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/core/BaseListFragment.kt
@@ -3,7 +3,7 @@ package com.ikmr.banbara23.yaeyama_liner_checker.core
 import android.content.Context
 import androidx.fragment.app.ListFragment
 
-open class BaseListFragment : androidx.fragment.app.ListFragment() {
+open class BaseListFragment : ListFragment() {
     override fun getContext(): Context? {
         return activity!!.applicationContext
     }
