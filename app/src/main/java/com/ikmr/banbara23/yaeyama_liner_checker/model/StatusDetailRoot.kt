7f79037ffa25ada93a3dcc9ee84f5900e9955ef8
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-03-08 08:37:29 +0900

    kotlin化

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/StatusDetailRoot.kt
@@ -0,0 +1,15 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.model
+
+import com.ikmr.banbara23.yaeyama_liner_checker.model.time_table.TimeTable
+
+/**
+ * ステータス詳細を表示するルートモデル
+ *
+ *
+ * RxJavaのzipで使うために作成した
+ */
+data class StatusDetailRoot(
+    val portStatus: PortStatus,
+    val detailLinerInfo: DetailLinerInfo,
+    val timeTable: TimeTable
+)
