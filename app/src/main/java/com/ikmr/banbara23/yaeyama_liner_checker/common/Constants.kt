25883c656517c3a4047b581734fb4f7d329697bd
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-03-08 08:59:05 +0900

    ConstantsをKotlin化

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/Constants.kt
@@ -0,0 +1,29 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.common
+
+/**
+ * 定数クラス
+ */
+object Constants {
+
+    // 天気URL
+    const val weatherUrl = "http://weather.yahoo.co.jp/weather/jp/47/9410.html"
+    
+    const val BUNDLE_KEY_DETAIL = "BUNDLE_KEY_DETAIL"
+    const val BUNDLE_KEY_COMPANY = "BUNDLE_KEY_COMPANY"
+    const val BUNDLE_KEY_PORT_CODE = "BUNDLE_KEY_PORT_CODE"
+    const val BUNDLE_KEY_PORT_NAME = "BUNDLE_KEY_PORT_NAME"
+}
+
+// FireBase Analytics
+object FireBaseAnalyticsTag {
+    const val TOP = "top"
+    const val OTHER = "other"
+    const val SETTING = "setting"
+    const val STATUS_LIST = "status_list"
+    const val STATUS_DETAIL_ANNEI = "status_detail_annei"
+    const val STATUS_DETAIL_YKF = "status_detail_ykf"
+    const val STATUS_DETAIL_DREAM = "status_detail_dream"
+    const val WEATHER = "weather"
+    const val TIME_TABLE = "time_table"
+    const val TIME_TABLE_SPINNER = "time_table_spinner"
+}
