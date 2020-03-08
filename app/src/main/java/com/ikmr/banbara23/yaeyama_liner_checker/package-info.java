78774e1e845df4a9ad21ee71b0254ce67f5975f0
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-03-08 09:39:34 +0900

    Epoxyライブラリを導入

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/package-info.java
@@ -0,0 +1,9 @@
+@EpoxyDataBindingLayouts({
+                                 R.layout.view_status_detail_top,
+                         })
+
+package com.ikmr.banbara23.yaeyama_liner_checker;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
+
+
