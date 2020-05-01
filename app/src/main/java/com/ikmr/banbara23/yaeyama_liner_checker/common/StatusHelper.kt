1ee0c15b5e9465bd930917ccb2d4c6b7998e205a
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-05-01 15:05:17 +0900

    topステータスのbackgroundに丸角xmlを適用した

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.kt
@@ -1,6 +1,10 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.common
 
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.core.Base
@@ -8,15 +12,27 @@ import com.ikmr.banbara23.yaeyama_liner_checker.core.Base
 object StatusHelper {
 
     @JvmStatic
-    fun getStatusColor(code: String?): Int {
-        if (code == null) {
-            return ContextCompat.getColor(Base.getContext(), R.color.status_cation)
-        }
+    fun getStatusColor(code: String?): Drawable? {
+        // if (code == null) {
+        //     return ContextCompat.getDrawable(
+        //         Base.getContext(), R.drawable.shape_rounded_corners_status_cation
+        //     )
+        // }
         return when (code) {
-            "normal" -> ContextCompat.getColor(Base.getContext(), R.color.status_normal)
-            "cancel", "suspend" -> ContextCompat.getColor(Base.getContext(), R.color.status_cancel)
-            "cation" -> ContextCompat.getColor(Base.getContext(), R.color.status_cation)
-            else -> ContextCompat.getColor(Base.getContext(), R.color.status_cation)
+            "normal" -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_nomal)
+            "cancel", "suspend" -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_cancel)
+            "cation" -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_cation)
+            else -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_cancel)
         }
     }
 }
+
+@BindingAdapter("statusBackground")
+fun setStatusBackground(view: TextView, statusCode: String?) {
+    statusCode ?: run {
+        Log.d("statusBackground", "statusCode is null")
+        return
+    }
+    Log.d("statusBackground", statusCode)
+}
+
