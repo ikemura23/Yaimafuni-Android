f53e3952ef2e7528623b759bfbac722436206cab
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-05-03 08:03:01 +0900

    開発用コードの削除

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/common/StatusHelper.kt
@@ -1,11 +1,7 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.common
 
 import android.graphics.drawable.Drawable
-import android.util.Log
-import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.databinding.BindingAdapter
-
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.core.Base
 
@@ -13,11 +9,6 @@ object StatusHelper {
 
     @JvmStatic
     fun getStatusColor(code: String?): Drawable? {
-        // if (code == null) {
-        //     return ContextCompat.getDrawable(
-        //         Base.getContext(), R.drawable.shape_rounded_corners_status_cation
-        //     )
-        // }
         return when (code) {
             "normal" -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_nomal)
             "cancel", "suspend" -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_cancel)
@@ -25,14 +16,5 @@ object StatusHelper {
             else -> ContextCompat.getDrawable(Base.getContext(), R.drawable.shape_rounded_corners_status_cancel)
         }
     }
-}
-
-@BindingAdapter("statusBackground")
-fun setStatusBackground(view: TextView, statusCode: String?) {
-    statusCode ?: run {
-        Log.d("statusBackground", "statusCode is null")
-        return
-    }
-    Log.d("statusBackground", statusCode)
 }
 
