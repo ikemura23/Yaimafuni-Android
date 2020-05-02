2281fc804db8291de9828abad20e606889971934
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-05-03 08:06:22 +0900

    リファクタリング

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/top/TopViewModel.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/top/TopViewModel.kt
@@ -19,19 +19,18 @@ class TopViewModel {
     var weather = ObservableField<String>()
     var showWeatherProgress = ObservableBoolean()
 
-    // 会社別ステータス
-    // var aneiColor = ObservableInt()
-    // var ykfColor = ObservableInt()
-
+    // ステータス文字
     var aneiStatus = ObservableField<String>()
     var ykfStatus = ObservableField<String>()
 
+    // ステータスの背景色（丸角）
+    var aneiBackground = ObservableField<Drawable>()
+    var ykfBackground = ObservableField<Drawable>()
+
+    // 読込中のプログレス表示
     var showCompanyProgress = ObservableBoolean()
 
     // 台風
     var typhoon = ObservableField<String>()
     var showTyphoonProgress = ObservableBoolean()
-
-    var aneiBackground = ObservableField<Drawable>()
-    var ykfBackground = ObservableField<Drawable>()
 }
