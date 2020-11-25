7469dc8edf2fe51636b1d46e5e24c2a89dd9a72b
banbara23 <8417910+banbara23@users.noreply.github.com>
2020-11-25 15:32:06 +0900

    ログ出力を追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/top/TopActivity.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/front/top/TopActivity.kt
@@ -2,9 +2,9 @@ package com.ikmr.banbara23.yaeyama_liner_checker.front.top
 
 import android.os.Bundle
 import com.google.android.play.core.review.ReviewManagerFactory
-
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.core.BaseActivity
+import timber.log.Timber
 
 class TopActivity : BaseActivity() {
 
@@ -20,15 +20,18 @@ class TopActivity : BaseActivity() {
         val request = manager.requestReviewFlow()
         request.addOnCompleteListener { request ->
             if (request.isSuccessful) {
+                Timber.d("successful")
                 // We got the ReviewInfo object
                 val reviewInfo = request.result
                 val flow = manager.launchReviewFlow(this, reviewInfo)
                 flow.addOnCompleteListener { _ ->
+                    Timber.d("onCompleteListener")
                     // The flow has finished. The API does not indicate whether the user
                     // reviewed or not, or even whether the review dialog was shown. Thus, no
                     // matter the result, we continue our app flow.
                 }
             } else {
+                Timber.d("not successful")
                 // There was some problem, continue regardless of the result.
             }
         }
