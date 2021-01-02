818caf956d715cde62f32881bcdffc328013c014
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-01-02 11:39:17 +0900

    WeatherInfo クラスをKotlin化

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/weather/WeatherInfo.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/weather/WeatherInfo.kt
@@ -1,34 +1,11 @@
-package com.ikmr.banbara23.yaeyama_liner_checker.model.weather;
+package com.ikmr.banbara23.yaeyama_liner_checker.model.weather
 
 /**
  * 天気ルート
  */
-public class WeatherInfo {
-    private Weather today;
-
-    private Weather tomorrow;
-
-    public Weather getToday() {
-        return today;
-    }
-
-    public void setToday(Weather today) {
-        this.today = today;
-    }
-
-    public Weather getTomorrow() {
-        return tomorrow;
-    }
-
-    public void setTomorrow(Weather tomorrow) {
-        this.tomorrow = tomorrow;
-    }
-
-    @Override
-    public String toString() {
-        return "WeatherInfo{" +
-               "today=" + today +
-               ", tomorrow=" + tomorrow +
-               '}';
-    }
-}
+data class WeatherInfo(
+    // 今日の天気
+    var today: Weather = Weather(),
+    // 明日の天気
+    var tomorrow: Weather = Weather()
+)
