0d997cabc620fde46869fce16c2ae212e242a6d1
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-01-02 09:58:26 +0900

    TemperatureクラスをJavaからKotlin化

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/weather/Temperature.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/model/weather/Temperature.kt
@@ -1,34 +1,11 @@
-package com.ikmr.banbara23.yaeyama_liner_checker.model.weather;
+package com.ikmr.banbara23.yaeyama_liner_checker.model.weather
 
 /**
  * 気温
  */
-public class Temperature {
-
-    private String hight;
-    private String low;
-
-    public String getHight() {
-        return this.hight;
-    }
-
-    public void setHight(String hight) {
-        this.hight = hight;
-    }
-
-    public String getLow() {
-        return this.low;
-    }
-
-    public void setLow(String low) {
-        this.low = low;
-    }
-
-    @Override
-    public String toString() {
-        return "Temperature{" +
-               "hight='" + hight + '\'' +
-               ", low='" + low + '\'' +
-               '}';
-    }
-}
+data class Temperature(
+    // 最高
+    var height: String = "",
+    // 最低
+    var low: String = ""
+)
