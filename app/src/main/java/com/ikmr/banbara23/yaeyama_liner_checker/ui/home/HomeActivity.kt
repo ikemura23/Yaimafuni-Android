0129a58752181442b06e92a44ca85b2c64c45d71
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-01-03 18:11:17 +0900

    Bottom Navigation Activityを作成してみた（Android Studioの機能）

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.ikmr.banbara23.yaeyama_liner_checker.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        // setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.ui.home.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
