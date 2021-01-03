268c29d1c81064baad32eb77d6bf9f29e86f80d3
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-01-03 18:13:31 +0900

    リファクタリング

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/HomeActivity.kt
@@ -3,12 +3,13 @@ package com.ikmr.banbara23.yaeyama_liner_checker.ui.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 
+/**
+ * ホーム画面、Bottom NavigationのあるActivity
+ */
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +18,6 @@ class HomeActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
