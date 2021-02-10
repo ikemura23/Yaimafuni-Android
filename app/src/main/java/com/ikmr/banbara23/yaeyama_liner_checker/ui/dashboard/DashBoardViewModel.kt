1b92c2bd574e7cf818b92188e3d165a2d3566cff
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-02-11 08:49:50 +0900

    DashBoardFragment ViewModelを作った

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardViewModel.kt
@@ -0,0 +1,10 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * トップに表示するステータスのダッシュボード ViewModel
+ */
+class DashBoardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
