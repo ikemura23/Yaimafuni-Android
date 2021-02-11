fa32b79957ef403627204d73b8a432ef663bcb2a
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-02-11 11:26:00 +0900

    findNavControllerだとエラーが起きるので修正

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/home/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.ikmr.banbara23.yaeyama_liner_checker.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ikmr.banbara23.yaeyama_liner_checker.R
@@ -14,10 +14,10 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_activity)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        navView.setupWithNavController(navController)
+        // val navController = findNavController(R.id.nav_host_fragment) <= ビルドは成功するが起動するとエラーとなる、↓で解決
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navView.setupWithNavController(navHostFragment.navController)
     }
 }
