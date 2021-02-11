4033a08331abb0ddfefd1c4b37d421bbfc16a578
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-02-11 10:48:13 +0900

    binding変数を作成

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
@@ -6,20 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.ikmr.banbara23.yaeyama_liner_checker.R
+import com.ikmr.banbara23.yaeyama_liner_checker.databinding.DashBoardFragmentBinding
 
 /**
  * トップに表示するステータスのダッシュボード画面
  */
 class DashBoardFragment : Fragment() {
-
+    private lateinit var binding: DashBoardFragmentBinding
     private lateinit var viewModel: DashBoardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.dash_board_fragment, container, false)
+    ): View {
+        binding = DashBoardFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
