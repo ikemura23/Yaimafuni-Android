8655d2087f51a4f93f6371f65e5cd64d7a40e2c9
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-02-12 13:58:25 +0900

    FragmentとViewModelを連結

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
@@ -5,27 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.DashBoardFragmentBinding
+import timber.log.Timber
 
 /**
  * トップに表示するステータスのダッシュボード画面
  */
 class DashBoardFragment : Fragment() {
     private lateinit var binding: DashBoardFragmentBinding
-    private lateinit var viewModel: DashBoardViewModel
+
+    private val viewModel by viewModels<DashBoardViewModelImpl>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DashBoardFragmentBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DashBoardViewModelImpl::class.java)
+        viewModel.uiState.observe(viewLifecycleOwner) {
+            Timber.d("$it")
+            binding.topPort = it
+        }
+        viewModel.fetchTopPortStatus()
     }
 
     companion object {
