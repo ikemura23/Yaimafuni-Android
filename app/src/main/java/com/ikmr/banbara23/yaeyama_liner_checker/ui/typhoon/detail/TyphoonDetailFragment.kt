74cccf3d78214dbb90df9c4ccea94e1446274a77
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-02-19 18:10:52 +0900

    台風詳細のリファクタリング

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/detail/TyphoonDetailFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/detail/TyphoonDetailFragment.kt
@@ -1,15 +1,13 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.typhoon.detail
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.common.Constants
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.TyphoonDetailFragmentBinding
+import com.ikmr.banbara23.yaeyama_liner_checker.ext.viewBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Typhoon
 import com.ikmr.banbara23.yaeyama_liner_checker.utils.CustomTabUtil
 import com.squareup.picasso.Picasso
@@ -18,31 +16,26 @@ import timber.log.Timber
 /**
  * 台風詳細 Fragment
  */
-class TyphoonDetailFragment : Fragment() {
-    private lateinit var binding: TyphoonDetailFragmentBinding
-    private val typhoon: Typhoon? by lazy {
+class TyphoonDetailFragment : Fragment(R.layout.typhoon_detail_fragment) {
+    private val binding: TyphoonDetailFragmentBinding by viewBinding()
+    private val argTyphoon: Typhoon by lazy {
         TyphoonDetailFragmentArgs.fromBundle(requireArguments()).typhoon
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.typhoon_detail_fragment, container, false)
-        binding.includeTitle.titleBar.setNavigationOnClickListener { findNavController().navigateUp() }
-        binding.includeTitle.title = typhoon?.name
-        binding.typhoon = typhoon
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // 画像読み込み
-        Timber.d(typhoon.toString())
-        typhoon?.let { Picasso.get().load(it.img).into(binding.image) }
-        // Webで見るボタン
-        binding.button.setOnClickListener {
-            CustomTabUtil.start(requireActivity(), Constants.TYPHOON_URL)
+        Timber.d(argTyphoon.toString())
+
+        with(binding) {
+            typhoon = argTyphoon
+            // タイトル設定
+            includeTitle.titleBar.setNavigationOnClickListener { findNavController().navigateUp() }
+            // Webで見るボタン
+            button.setOnClickListener {
+                CustomTabUtil.start(requireActivity(), Constants.TYPHOON_URL)
+            }
         }
+        // 画像読み込み
+        Picasso.get().load(argTyphoon.img).into(binding.image)
     }
 }
