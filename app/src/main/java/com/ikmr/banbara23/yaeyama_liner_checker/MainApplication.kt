fdc739b45839f245c3e5fc71c3cd04496643af7e
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-04-03 16:57:28 +0900

    move package MainApplication

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MainApplication.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/MainApplication.kt
@@ -1,7 +1,8 @@
-package com.ikmr.banbara23.yaeyama_liner_checker.core
+package com.ikmr.banbara23.yaeyama_liner_checker
 
 import android.app.Application
 import com.google.firebase.database.FirebaseDatabase
+import com.ikmr.banbara23.yaeyama_liner_checker.core.Base
 import timber.log.Timber
 
 class MainApplication : Application() {
