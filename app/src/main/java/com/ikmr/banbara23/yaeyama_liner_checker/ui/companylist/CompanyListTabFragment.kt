ef26b4c5fb0c47480b142e812a630a5edcc71e80
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-04 22:49:04 +0900

    viewBindingを使う in CompanyListTabFragment.kt

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/companylist/CompanyListTabFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/companylist/CompanyListTabFragment.kt
@@ -1,28 +1,18 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.companylist
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.FragmentCompanyListTabBinding
+import com.ikmr.banbara23.yaeyama_liner_checker.ext.viewBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.model.Company
 
-class CompanyListTabFragment : Fragment() {
-    private lateinit var binding: FragmentCompanyListTabBinding
+class CompanyListTabFragment : Fragment(R.layout.fragment_company_list_tab) {
+    private val binding: FragmentCompanyListTabBinding by viewBinding()
     private val company: Company by lazy {
         CompanyListTabFragmentArgs.fromBundle(requireArguments()).company
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_company_list_tab, container, false)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
