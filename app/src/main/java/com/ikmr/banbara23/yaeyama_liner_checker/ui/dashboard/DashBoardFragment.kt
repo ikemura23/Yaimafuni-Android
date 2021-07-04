12dc71e1d8b04947dc00e5b3a5b66b01e7356319
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-04 22:46:26 +0900

    viewBindingを使う

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardFragment.kt
@@ -1,32 +1,23 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.DashBoardFragmentBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.ext.observeEvent
+import com.ikmr.banbara23.yaeyama_liner_checker.ext.viewBinding
 import timber.log.Timber
 
 /**
  * トップに表示するステータスのダッシュボード画面
  */
-class DashBoardFragment : Fragment() {
-    private lateinit var binding: DashBoardFragmentBinding
+class DashBoardFragment : Fragment(R.layout.dash_board_fragment) {
+    private val binding: DashBoardFragmentBinding by viewBinding()
 
     private val viewModel by viewModels<DashBoardViewModelImpl>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = DashBoardFragmentBinding.inflate(inflater)
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
