30db42d95867e12b505fd5448650650347e1cdad
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-07 10:07:31 +0900

    fabは不要なので消す

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailActivity.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailActivity.kt
@@ -3,8 +3,6 @@ package com.ikmr.banbara23.yaeyama_liner_checker.ui.portstatusdetail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.PortStatusDetailActivityBinding
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.portstatusdetail.ui.main.SectionsPagerAdapter
@@ -35,11 +33,5 @@ class PortStatusDetailActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
