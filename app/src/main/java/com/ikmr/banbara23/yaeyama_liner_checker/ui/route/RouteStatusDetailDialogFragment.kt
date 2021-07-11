6851b18eb6aed47c73a81c8e6d1d9453a555b6d1
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-11 13:57:00 +0900

    RouteStatusDetailDialogFragment.kt を新規作成

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/route/RouteStatusDetailDialogFragment.kt
@@ -0,0 +1,10 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.ui.route
+
+import androidx.fragment.app.DialogFragment
+import com.ikmr.banbara23.yaeyama_liner_checker.R
+
+/**
+ * ルールの運行ステータス画面 Dialog
+ */
+class RouteStatusDetailDialogFragment : DialogFragment(R.layout.route_status_detail_dialog_fragment) {
+}
