e801a34ad0dd9a04a3953a5c3c676dedef60ca56
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 18:54:45 +0900

    fetchStatusDetail にFirebase取得を実装

--- shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
+++ shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
@@ -1,12 +1,27 @@
 package com.ikemura.shared.repository
 
+import com.ikemura.shared.model.statusdetail.Company
 import com.ikemura.shared.model.statusdetail.PortStatus
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.app
+import dev.gitlive.firebase.database.database
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
 class StatusDetailRepository {
     /**
      * 運行情報の詳細を取得する
      */
-    fun fetchStatusDetail() {
+    @InternalCoroutinesApi
+    fun fetchStatusDetail(company: Company, portCode: String): Flow<UiState> = flow {
+        val path = "${company.code}/$portCode"
+        val dbRef = Firebase.database(Firebase.app).reference(path)
+        dbRef.valueEvents.collect {
+            val portStatus = it.value(PortStatus.serializer())
+            emit(UiState.Success(portStatus))
+        }
     }
 
     sealed class UiState {
