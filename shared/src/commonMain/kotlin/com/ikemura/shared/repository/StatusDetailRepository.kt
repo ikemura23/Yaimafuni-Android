d59f98e891bfa742c048163ea31e958583c4e135
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 18:22:33 +0900

    StatusDetailRepository.UiState を作成

--- shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
+++ shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
@@ -1,9 +1,16 @@
 package com.ikemura.shared.repository
 
+import com.ikemura.shared.model.statusdetail.PortStatus
+
 class StatusDetailRepository {
     /**
      * 運行情報の詳細を取得する
      */
     fun fetchStatusDetail() {
+    }
+
+    sealed class UiState {
+        data class Success(val data: PortStatus) : UiState()
+        data class Error(val error: Throwable) : UiState()
     }
 }
