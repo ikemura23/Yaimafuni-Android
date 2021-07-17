1b1a240b19aabace6107159408531b0127fa9af9
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 19:07:20 +0900

    UiState.kt を作成

--- shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
+++ shared/src/commonMain/kotlin/com/ikemura/shared/repository/StatusDetailRepository.kt
@@ -4,6 +4,7 @@ import com.ikemura.shared.model.statusdetail.Company
 import com.ikemura.shared.model.statusdetail.PortStatus
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.app
+import dev.gitlive.firebase.database.FirebaseDatabase
 import dev.gitlive.firebase.database.database
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -11,13 +12,16 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 
 class StatusDetailRepository {
+    // Firebase Realtime Database
+    private var database: FirebaseDatabase = Firebase.database(Firebase.app)
+
     /**
      * 運行情報の詳細を取得する
      */
     @InternalCoroutinesApi
-    fun fetchStatusDetail(company: Company, portCode: String): Flow<UiState> = flow {
+    fun fetchStatusDetail(company: Company, portCode: String): Flow<UiState<PortStatus>> = flow {
         val path = "${company.code}/$portCode"
-        val dbRef = Firebase.database(Firebase.app).reference(path)
+        val dbRef = database.reference(path)
         dbRef.valueEvents.collect {
             val portStatus = it.value(PortStatus.serializer())
             emit(UiState.Success(portStatus))
