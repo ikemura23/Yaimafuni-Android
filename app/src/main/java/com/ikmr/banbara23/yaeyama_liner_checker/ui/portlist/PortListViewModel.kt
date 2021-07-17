c4b644c76872756d813a9b47104d21170d95dcdc
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 18:20:17 +0900

    PortList系のクラスを削除

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portlist/PortListViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.ikmr.banbara23.yaeyama_liner_checker.ui.portlist
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel;
-import com.ikemura.shared.model.statusdetail.PortStatus
-
-class PortListViewModel : ViewModel() {
-    val items = MutableLiveData<List<PortStatus>>()
-}
