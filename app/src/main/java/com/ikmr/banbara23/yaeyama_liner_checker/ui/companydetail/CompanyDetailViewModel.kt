a5870191e8201fda35e23e3c845169907ed998ac
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 18:15:27 +0900

    CompanyDetail系のクラスを削除

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/companydetail/CompanyDetailViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ikmr.banbara23.yaeyama_liner_checker.ui.companydetail
-
-import androidx.lifecycle.ViewModel
-
-class CompanyDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
