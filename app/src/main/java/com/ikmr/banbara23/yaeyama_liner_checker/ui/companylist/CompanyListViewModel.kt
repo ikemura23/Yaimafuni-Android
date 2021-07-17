0f91fd3a13cd1278074e8b0cfcdc6379efe87f1c
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 17:33:56 +0900

    ApiClient#getCompanyStatus を消すため使っていない画面のAPI呼び出しをコメントアウト

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/companylist/CompanyListViewModel.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/companylist/CompanyListViewModel.kt
@@ -3,34 +3,30 @@ package com.ikmr.banbara23.yaeyama_liner_checker.ui.companylist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.ikmr.banbara23.yaeyama_liner_checker.api.ApiClient
-import com.ikmr.banbara23.yaeyama_liner_checker.model.Company
 import com.ikmr.banbara23.yaeyama_liner_checker.model.CompanyStatus
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subscribers.ResourceSubscriber
-import timber.log.Timber
 
 class CompanyListViewModel : ViewModel() {
     private val _companyStatus = MutableLiveData<CompanyStatus>()
     val companyStatus: LiveData<CompanyStatus> = _companyStatus
     private val disposable = CompositeDisposable()
 
-    fun load(company: Company) {
-        disposable.add(
-            ApiClient()
-                .getCompanyStatus(company.code)
-                .subscribeWith(object : ResourceSubscriber<CompanyStatus>() {
-                    override fun onComplete() {
-                    }
-
-                    override fun onNext(companyStatus: CompanyStatus) {
-                        _companyStatus.value = companyStatus
-                    }
-
-                    override fun onError(t: Throwable) {
-                        Timber.e(t)
-                    }
-                })
-        )
-    }
+    // fun load(company: Company) {
+    //     disposable.add(
+    //         ApiClient()
+    //             .getCompanyStatus(company.code)
+    //             .subscribeWith(object : ResourceSubscriber<CompanyStatus>() {
+    //                 override fun onComplete() {
+    //                 }
+    //
+    //                 override fun onNext(companyStatus: CompanyStatus) {
+    //                     _companyStatus.value = companyStatus
+    //                 }
+    //
+    //                 override fun onError(t: Throwable) {
+    //                     Timber.e(t)
+    //                 }
+    //             })
+    //     )
+    // }
 }
