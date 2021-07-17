9ea33e4d4f7dbb389603c90b523ece4d0b9ed132
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-17 22:32:11 +0900

    GetStatusDetail.kt を作成

---
+++ shared/src/commonMain/kotlin/com/ikemura/shared/usecase/GetStatusDetail.kt
@@ -0,0 +1,31 @@
+package com.ikemura.shared.usecase
+
+import com.ikemura.shared.model.statusdetail.Company
+import com.ikemura.shared.model.statusdetail.PortStatus
+import com.ikemura.shared.model.statusdetail.StatusDetailResult
+import com.ikemura.shared.repository.StatusDetailRepository
+import com.ikemura.shared.repository.UiState
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.zip
+
+@InternalCoroutinesApi
+class GetStatusDetail(
+    private val statusDetailRepository: StatusDetailRepository,
+) {
+    suspend operator fun invoke(company: Company, portCode: String): Flow<UiState<StatusDetailResult>> {
+        val portStatusFlow = statusDetailRepository.fetchStatusDetail(company, portCode)
+        val timeTableFlow = statusDetailRepository.fetchTimeTable(company, portCode)
+        return portStatusFlow.zip(timeTableFlow) { portStatusState: UiState<PortStatus>, timeTableState ->
+            return@zip if (portStatusState is UiState.Success && timeTableState is UiState.Success) {
+                val result = StatusDetailResult(
+                    portStatusState.data,
+                    timeTableState.data
+                )
+                UiState.Success(result)
+            } else {
+                UiState.Error(Exception())
+            }
+        }
+    }
+}
