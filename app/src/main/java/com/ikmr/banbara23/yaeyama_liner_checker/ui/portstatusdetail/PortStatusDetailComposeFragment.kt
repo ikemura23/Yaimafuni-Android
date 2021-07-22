c84404b4c633774323272bc25915af930cf2e06d
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-07-22 19:31:30 +0900

    feat: PortStatusDetailComposeFragment にComposeを使う

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailComposeFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailComposeFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 class PortStatusDetailComposeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // FragmentにComposeを使う書き方
+        return ComposeView(requireContext()).apply {
+            setContent {
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+            }
+        }
     }
 }
