095321996786e98ba5e12a584c7d6f1855f57b0b
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-08-06 17:19:51 +0900

    feat: ステータスの色を付けてみた

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailComposeFragment.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/portstatusdetail/PortStatusDetailComposeFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +55,13 @@ private fun Screen() {
             )
             Text(
                 text = "ステータス",
+                color = Color.White,
+                modifier = Modifier
+                    .background(
+                        color = Color.Blue,
+                        shape = RoundedCornerShape(16)
+                    )
+                    .padding(vertical = 4.dp, horizontal = 8.dp)
             )
         }
 
