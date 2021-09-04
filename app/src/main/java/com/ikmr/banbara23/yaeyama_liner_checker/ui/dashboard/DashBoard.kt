e05f45c124332a7df6e0be3584a7bd8700739709
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-04 20:50:47 +0900

    feat: DashBoardPreviewメソッド を作成

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
@@ -1,7 +1,20 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
 fun DashBoard() {
+}
+
+@Preview
+@Composable
+fun DashBoardPreview() {
+    YaimafuniAndroidTheme {
+        Surface {
+            DashBoard()
+        }
+    }
 }
