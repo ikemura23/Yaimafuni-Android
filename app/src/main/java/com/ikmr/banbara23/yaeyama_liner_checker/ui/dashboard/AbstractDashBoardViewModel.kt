396625e058106649e04be4fef6b8ea7125c2b553
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-04 20:33:33 +0900

    refact: 不要になった処理を削除

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/AbstractDashBoardViewModel.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/AbstractDashBoardViewModel.kt
@@ -3,11 +3,9 @@ package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.ikemura.shared.model.top.Ports
-import com.ikemura.shared.model.top.TopPort
 import com.ikmr.banbara23.yaeyama_liner_checker.core.Event
 
 abstract class DashBoardViewModel : ViewModel() {
-    abstract val uiState: LiveData<TopPort>
     abstract val nav: LiveData<Event<DashBoardViewModelImpl.Nav>>
 
     abstract fun onClickPort(ports: Ports?)
--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardViewModel.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoardViewModel.kt
@@ -1,19 +1,13 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.ikemura.shared.model.top.Ports
 import com.ikemura.shared.model.top.TopPort
 import com.ikemura.shared.repository.TopStatusRepository
 import com.ikemura.shared.repository.UiState
 import com.ikmr.banbara23.yaeyama_liner_checker.core.Event
 import com.ikmr.banbara23.yaeyama_liner_checker.core.toEvent
-import com.ikmr.banbara23.yaeyama_liner_checker.repository.TopPortStatusRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 /**
@@ -21,29 +15,9 @@ import timber.log.Timber
  */
 class DashBoardViewModelImpl : DashBoardViewModel() {
 
-    override val uiState = MutableLiveData<TopPort>()
     override val nav = MutableLiveData<Event<Nav>>()
 
-    private val database: DatabaseReference by lazy {
-        FirebaseDatabase.getInstance().reference.ref.child("top_port")
-    }
-
-    private val topPortStatusRepository: TopPortStatusRepository by lazy {
-        TopPortStatusRepository(database)
-    }
-
     private val topStatusRepository = TopStatusRepository()
-
-    fun fetchTopPortStatus() {
-        viewModelScope.launch {
-            topPortStatusRepository.getTopPortStatus().collect { state ->
-                when (state) {
-                    is TopPortStatusState.Success -> uiState.value = state.topPort
-                    else -> Timber.e("$state")
-                }
-            }
-        }
-    }
 
     fun fetchTopStatus(): Flow<UiState<TopPort>> {
         return topStatusRepository.fetchTopStatus()
