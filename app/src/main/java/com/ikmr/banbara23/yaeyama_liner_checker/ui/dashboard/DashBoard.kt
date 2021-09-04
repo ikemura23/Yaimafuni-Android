4a1a4a22bd4f6a3677cd2981cf36bc8a73a4c217
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-04 20:52:17 +0900

    feat: Card を追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
@@ -1,12 +1,20 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
 fun DashBoard() {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+    }
 }
 
 @Preview
