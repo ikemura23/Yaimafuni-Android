eca69ba3711b5d741e3ec3f591d041014d004764
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-05 01:07:31 +0900

    feat: DashBoardHeader を作成

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
@@ -1,12 +1,24 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.dashboard
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
@@ -17,6 +29,26 @@ fun DashBoard() {
         Column {
 
         }
+    }
+}
+
+@Preview(name = "ヘッダー")
+@Composable
+fun DashBoardHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Spacer(modifier = Modifier.padding(8.dp))
+        Icon(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = R.drawable.ic_harbor),
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Text(text = stringResource(id = R.string.dash_board_title))
     }
 }
 
