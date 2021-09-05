0986ffcf5f2c0d8d3aede50a344a202e5d11ab9f
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-05 23:33:19 +0900

    feat: ライブラリをアップデート

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
                 implementation(
                     "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion"
                 )
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.1")
                 // firebase
                 implementation("dev.gitlive:firebase-database:1.3.1")
                 // Koin core features
