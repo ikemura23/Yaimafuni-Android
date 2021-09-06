6ca38e73093f54b48c524272819484ff8eecf018
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-06 14:36:04 +0900

    feat: FakeDashBoardDataProvider.kt を作成した（実験）

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/dashboard/DashBoard.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ikemura.shared.model.statusdetail.PortStatus
 import com.ikemura.shared.model.statusdetail.Status
 import com.ikemura.shared.model.top.Ports
 import com.ikmr.banbara23.yaeyama_liner_checker.R
@@ -89,7 +88,7 @@ fun DashBoardHeader() {
 @Preview
 @Composable
 fun DashBoardPreview() {
-    val dummyPorts: List<Ports> = (1..5).toList().map { dummyPort } + dummyPort2
+    val dummyPorts = FakeDashBoardDataProvider.dummyPortList
     YaimafuniAndroidTheme {
         Surface {
             DashBoard(dummyPorts) {}
@@ -188,50 +187,8 @@ fun DashBoardRowPreview() {
     YaimafuniAndroidTheme {
         Surface {
             DashBoardRow(
-                dummyPort
+                FakeDashBoardDataProvider.dummyPort1
             ) {}
         }
     }
 }
-
-private val dummyPort = Ports(
-    anei = PortStatus(
-        portCode = "hatoma",
-        portName = "鳩間島航路",
-        comment = "海上時化の為、全便欠航。",
-        status = Status(
-            text = "通常運航",
-            code = "normal",
-        )
-    ),
-    ykf = PortStatus(
-        portCode = "hatoma",
-        portName = "鳩間",
-        comment = "海上時化の為、全便欠航。",
-        status = Status(
-            text = "欠航",
-            code = "cancel",
-        )
-    )
-)
-
-private val dummyPort2 = Ports(
-    anei = PortStatus(
-        portCode = "hatoma",
-        portName = "上原航路",
-        comment = "海上時化の為、全便欠航。",
-        status = Status(
-            text = "通常運航",
-            code = "normal",
-        )
-    ),
-    ykf = PortStatus(
-        portCode = "hatoma",
-        portName = "上原",
-        comment = "海上時化の為、全便欠航。",
-        status = Status(
-            text = "欠航",
-            code = "cancel",
-        )
-    )
-)
