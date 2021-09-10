34b75b97a4bd44912f7951debb277b00f806fdf8
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-10 17:44:26 +0900

    feat: MainViewModelのtyphoonRepositoryを新しいのに入れ替えた

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/main/MainViewModel.kt
@@ -1,29 +1,17 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.main
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.database.FirebaseDatabase
-import com.ikmr.banbara23.yaeyama_liner_checker.repository.TyphoonRepositoryOld
-import com.ikmr.banbara23.yaeyama_liner_checker.repository.TyphoonUiState
+import com.ikemura.shared.model.tyhoon.Typhoon
+import com.ikemura.shared.repository.TyphoonRepositoryImpl
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class MainViewModel : ViewModel() {
-    private val typhoonRepositoryOld: TyphoonRepositoryOld by lazy {
-        // TODO: DI化したい
-        val databaseReference = FirebaseDatabase.getInstance().reference.ref.child("typhoon/tenkijp")
-        TyphoonRepositoryOld(databaseReference)
-    }
+    private val typhoonRepository = TyphoonRepositoryImpl()
 
     fun existsTyphoon(): Flow<Boolean> {
-        return typhoonRepositoryOld.getTyphoonList().map { state ->
-            when (state) {
-                is TyphoonUiState.Success -> {
-                    state.typhoonList.isNotEmpty()
-                }
-                else -> {
-                    false
-                }
-            }
+        return typhoonRepository.fetchTyphoonList().map { list: List<Typhoon> ->
+            list.isNotEmpty()
         }
     }
 }
