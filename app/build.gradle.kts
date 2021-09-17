f337b17c5b28dfc2a3799f292c20bab5080e3bfd
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-17 15:09:30 +0900

    fix: vectordrawable を削除 たぶんもう不要

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     // android
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.browser:browser:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
