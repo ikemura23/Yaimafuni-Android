f31b9b589aa831588eab00cb178a48097d08f4af
banbara23 <8417910+banbara23@users.noreply.github.com>
2021-09-17 14:40:20 +0900

    feat: Versions.kt に他のバージョン変数を追加

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,8 @@
 object Versions {
     const val gradlePlugin = "7.0.2"
+    const val kotlin = "1.5.21"
+    const val compose = "1.0.1"
+    const val navigation = "2.3.5"
+    const val firebaseCrashlyticsGradle = "2.7.1"
+    const val googleServices = "4.3.10"
 }
