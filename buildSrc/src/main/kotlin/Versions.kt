d1cb8e0bc80a836514aac41a64c62e65d01fbe67
ikemura23 <banbara.work@gmail.com>
2022-03-25 09:48:17 +0900

    chore: navigationを2.4.1にあげた

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val gradlePlugin = "7.1.2"
     const val kotlin = "1.5.21"
     const val compose = "1.0.1"
-    const val navigation = "2.3.5"
+    const val navigation = "2.4.1"
     const val firebaseCrashlyticsGradle = "2.7.1"
     const val googleServices = "4.3.10"
     const val coroutine = "1.5.1"
