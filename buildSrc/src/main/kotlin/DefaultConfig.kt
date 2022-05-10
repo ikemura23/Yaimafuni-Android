22c3c20c98a6695c4cf4fa8d6b3280c8b40f0589
ikemura23 <banbara.work@gmail.com>
2022-05-10 21:31:08 +0900

    update gradlePlugin 7.1.3

--- buildSrc/src/main/kotlin/DefaultConfig.kt
+++ buildSrc/src/main/kotlin/DefaultConfig.kt
@@ -1,6 +1,6 @@
 object DefaultConfig {
     // https://developer.android.com/guide/topics/manifest/uses-sdk-element
-    const val compileSdk = 30 // Android 11	30	R
+    const val compileSdk = 31 // Android 11	30	R
     const val minSdk = 24 //Android 7.0	24	N
-    const val targetSdk = 30
+    const val targetSdk = 31
 }
