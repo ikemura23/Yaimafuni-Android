571401c1cae98715063b7b49025bc9bb97c57918
ikemura23 <banbara.work@gmail.com>
2022-05-15 23:32:03 +0900

    feat: TyphoonListScreen.kt を新規作成

---
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TyphoonListScreen.kt
@@ -0,0 +1,20 @@
+package com.ikmr.banbara23.yaeyama_liner_checker.ui.typhoon.list
+
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
+
+@Composable
+fun TyphoonListScreen() {
+}
+
+@Preview
+@Composable
+fun TyphoonListScreenPreview() {
+    YaimafuniAndroidTheme {
+        Surface {
+            TyphoonListScreen()
+        }
+    }
+}
