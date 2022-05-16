3fbafbb8cb6d7fc98dfe86d0a33323c44496984e
ikemura23 <banbara.work@gmail.com>
2022-05-17 07:16:09 +0900

    feat: TitleSection.kt を新規作成

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TitleSection.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TitleSection.kt
@@ -1,27 +1,23 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.typhoon.list
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
-/**
- * Composeの台風一覧画面
- */
 @Composable
-fun TyphoonListScreen() {
-    Column {
-        TitleSection()
-    }
+fun TitleSection() {
+    TopAppBar(title = { Text(text = "台風情報") })
 }
 
 @Preview
 @Composable
-fun TyphoonListScreenPreview() {
+fun TitleSectionPreview() {
     YaimafuniAndroidTheme {
         Surface {
-            TyphoonListScreen()
+            TitleSection()
         }
     }
 }
