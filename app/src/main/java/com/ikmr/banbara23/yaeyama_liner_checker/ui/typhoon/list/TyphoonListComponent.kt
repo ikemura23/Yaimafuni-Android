687ed6899e3d73a2bc6276219b11abe93d74cf9d
ikemura23 <banbara.work@gmail.com>
2022-05-23 23:17:29 +0900

    feat: TyphoonListContent に CardとLazyColumnを追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TyphoonListComponent.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TyphoonListComponent.kt
@@ -1,7 +1,9 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.typhoon.list
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,13 +14,18 @@ import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
 fun TyphoonListContent() {
-    Column(
-        modifier = Modifier.padding(16.dp)
+    Card(
+        modifier = Modifier.padding(16.dp),
+        shape = RoundedCornerShape(8.dp),
     ) {
-        Text(text = "アイテム 1")
-        Text(text = "アイテム 2")
-        Text(text = "アイテム 3")
-        Text(text = "アイテム 4")
+        LazyColumn() {
+            items(5) {
+                Text(text = "アイテム 1")
+                Text(text = "アイテム 2")
+                Text(text = "アイテム 3")
+                Text(text = "アイテム 4")
+            }
+        }
     }
 }
 
