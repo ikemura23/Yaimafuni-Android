7a7002eedecce0fbfbd52963dcf19bee3c43c20d
ikemura23 <banbara.work@gmail.com>
2022-05-24 16:51:12 +0900

    feat: TyphoonListItemComponent に 仮のColumnとRowを追加

--- app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TyphoonListItemComponent.kt
+++ app/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/typhoon/list/TyphoonListItemComponent.kt
@@ -1,11 +1,12 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.typhoon.list
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,6 +18,16 @@ import com.ikmr.banbara23.yaeyama_liner_checker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
 fun TyphoonListItemComponent() {
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            TyphoonName("台風18号(ミートク)")
+            UpdatedTime("2022/05/24 現在")
+        }
+    }
 }
 
 @Preview
