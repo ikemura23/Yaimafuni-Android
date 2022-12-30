752a75c89f978412c667fa43f237b33332133ed1
ikemura23 <banbara.work@gmail.com>
2022-12-30 20:31:33 +0900

    refact: コンポーネントを別ファイルに切り出す

--- androidApp/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/weather/compose/WeatherListItem.kt
+++ androidApp/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/weather/compose/WeatherListItem.kt
@@ -1,24 +1,12 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.weather.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.yaeyama_liner_checker.domain.weather.Weather
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -35,58 +23,8 @@ fun WeatherListItem(
         Column(
             modifier = modifier.fillMaxWidth()
         ) {
-            CardHeader(weather.date)
-            CardContent(weather)
-        }
-    }
-}
-
-@Composable
-fun CardHeader(title: String) {
-    Text(
-        text = title,
-        modifier = Modifier.background(color = colorResource(id = R.color.primary)).fillMaxWidth()
-            .padding(16.dp),
-        style = TextStyle.Default.copy(color = Color.White)
-    )
-}
-
-@Composable
-fun CardContent(weather: Weather) {
-    // 天気情報（天気・気温・風・波）
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("天気：")
-            Text(weather.weather)
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("気温：")
-            Text("最高: ${weather.temperature.hight}℃ 最低: ${weather.temperature.low}℃")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("風：")
-            Text(weather.wind)
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("波：")
-            Text(weather.wave)
+            WeatherListItemCardHeader(weather.date)
+            WeatherListItemCardContent(weather)
         }
     }
 }
@@ -98,16 +36,4 @@ private fun WeatherListItemPreview() {
         weather = Weather(),
         onItemClick = {}
     )
-}
-
-@Preview
-@Composable
-fun CardHeaderPreview() {
-    CardHeader("タイトル")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun CardContentPreview() {
-    CardContent(Weather())
 }
--- androidApp/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/weather/compose/WeatherScreen.kt
+++ androidApp/src/main/java/com/ikmr/banbara23/yaeyama_liner_checker/ui/weather/compose/WeatherScreen.kt
@@ -1,24 +1,14 @@
 package com.ikmr.banbara23.yaeyama_liner_checker.ui.weather.compose
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.weather.WeatherScreenViewModel
 import com.ikmr.banbara23.yaeyama_liner_checker.ui.weather.WeatherUiState
@@ -50,46 +40,10 @@ private fun WeatherScreen(uiState: WeatherUiState, onMoreClick: () -> Unit) {
     }
 }
 
-@Composable
-private fun WeatherPage(modifier: Modifier = Modifier, uiState: WeatherUiState) {
-    Box(
-        modifier = modifier
-            .background(color = Color.Transparent)
-            .fillMaxSize()
-    ) {
-        if (uiState is WeatherUiState.Success) {
-            LazyColumn(
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                // TODO: 固定のアイテム数をAPIレスポンスに直す
-                val values = listOf(uiState.weather.today, uiState.weather.tomorrow)
-                items(values) { weather ->
-                    WeatherListItem(
-                        weather = weather,
-                        onItemClick = {}
-                    )
-                }
-            }
-        }
-        if (uiState is WeatherUiState.Loading) {
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
-            )
-        }
-    }
-}
-
 @Preview
 @Composable
 private fun WeatherScreenPreview() {
     WeatherScreen(
         WeatherUiState.Loading
     ) {}
-}
-
-@Preview(showBackground = true)
-@Composable
-fun WeatherPagePreview() {
-    WeatherPage(uiState = WeatherUiState.Loading)
 }
