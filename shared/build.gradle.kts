b52968db5182b07222c02322f2a0e3cd51ded9dc
ikemura23 <banbara.work@gmail.com>
2023-02-07 08:32:07 +0900

    fix: ios環境のエラーが出るのでコメントアウト

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,15 +7,15 @@ plugins {
 kotlin {
     android()
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-        }
-    }
+    // listOf(
+    //     iosX64(),
+    //     iosArm64(),
+    //     iosSimulatorArm64()
+    // ).forEach {
+    //     it.binaries.framework {
+    //         baseName = "shared"
+    //     }
+    // }
 
     sourceSets {
         val commonMain by getting {
@@ -44,24 +44,24 @@ kotlin {
                 implementation("junit:junit:${Versions.junit}")
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
+        // val iosX64Main by getting
+        // val iosArm64Main by getting
+        // val iosSimulatorArm64Main by getting
+        // val iosMain by creating {
+        //     dependsOn(commonMain)
+        //     iosX64Main.dependsOn(this)
+        //     iosArm64Main.dependsOn(this)
+        //     iosSimulatorArm64Main.dependsOn(this)
+        // }
+        // val iosX64Test by getting
+        // val iosArm64Test by getting
+        // val iosSimulatorArm64Test by getting
+        // val iosTest by creating {
+        //     dependsOn(commonTest)
+        //     iosX64Test.dependsOn(this)
+        //     iosArm64Test.dependsOn(this)
+        //     iosSimulatorArm64Test.dependsOn(this)
+        // }
     }
 }
 
