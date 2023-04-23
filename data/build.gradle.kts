6d065950428e1d0b36e7d2a5dbf06ca7c74285b7
ikemura23 <banbara.work@gmail.com>
2023-04-24 07:56:06 +0900

    feat: dataモジュールにbuild.gradleを作成

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,12 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutine}")
+    // implementation("dev.gitlive:firebase-database:${Versions.firebaseDatabase}")
+    implementation("io.insert-koin:koin-core:${Versions.koin}")
+    implementation(project(":domain"))
 }
