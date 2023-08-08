52b3ce62c637eeed8572dbf4f33a32e6314ba305
ikemura23 <banbara.work@gmail.com>
2023-08-08 11:34:29 +0900

    feat: version 4.6.1のリリース準備

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.banbara.yaeyama.liner.checker"
         minSdk = libs.versions.app.minSdk.get().toInt()
         targetSdk = libs.versions.app.targetSdk.get().toInt()
-        versionCode = 87
-        versionName = "4.6.0"
+        versionCode = 88
+        versionName = "4.6.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
