9a339a19ed1990b8dfb49d5b9c7cd58ef708066d
ikemura23 <banbara.work@gmail.com>
2023-08-09 08:16:26 +0900

    feat: DashBoardViewModel のDIを activityViewModelに変更した

--- androidApp/src/main/java/com/yaeyama/linerchecker/di/ViewModelModule.kt
+++ androidApp/src/main/java/com/yaeyama/linerchecker/di/ViewModelModule.kt
@@ -1,10 +1,18 @@
 package com.yaeyama.linerchecker.di
 
+import com.yaeyama.linerchecker.ui.dashboard.DashBoardViewModel
 import com.yaeyama.linerchecker.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+/**
+ * https://insert-koin.io/docs/reference/koin-android/viewmodel/
+ */
 val viewModelModule = module {
     single {
         MainViewModel(get())
+    }
+    viewModel {
+        DashBoardViewModel()
     }
 }
