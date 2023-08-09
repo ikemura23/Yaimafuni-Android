536d5ca84653963d928a90218042a03c39d87bf5
ikemura23 <banbara.work@gmail.com>
2023-08-09 09:40:28 +0900

    chore: PortStatusDetailFragment の不要コードの削除

--- androidApp/src/main/java/com/yaeyama/linerchecker/ui/portstatusdetail/PortStatusDetailFragment.kt
+++ androidApp/src/main/java/com/yaeyama/linerchecker/ui/portstatusdetail/PortStatusDetailFragment.kt
@@ -1,21 +1,16 @@
 package com.yaeyama.linerchecker.ui.portstatusdetail
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.ikmr.banbara23.yaeyama_liner_checker.R
 import com.ikmr.banbara23.yaeyama_liner_checker.databinding.StatusDetailFragmentBinding
 import com.yaeyama.linerchecker.common.Constants.BUNDLE_KEY_COMPANY
 import com.yaeyama.linerchecker.common.Constants.BUNDLE_KEY_PORT_CODE
 import com.yaeyama.linerchecker.ext.viewBinding
 import com.yaeyama.linerchecker.repository.UiState
-import com.yaeyama.linerchecker.utils.CustomTabUtil
 import com.yaeyama_liner_checker.domain.statusdetail.Company
 import com.yaeyama_liner_checker.domain.statusdetail.StatusDetailResult
 import timber.log.Timber
@@ -27,11 +22,6 @@ class PortStatusDetailFragment : Fragment(R.layout.status_detail_fragment) {
     private val binding: StatusDetailFragmentBinding by viewBinding()
     private val viewModel: PortStatusDetailViewModel by lazy {
         ViewModelProvider(this).get(PortStatusDetailViewModel::class.java)
-    }
-    private val firebaseAnalytics: FirebaseAnalytics by lazy {
-        FirebaseAnalytics.getInstance(
-            requireActivity(),
-        )
     }
 
     /** パラメータ取得 会社 */
@@ -65,58 +55,6 @@ class PortStatusDetailFragment : Fragment(R.layout.status_detail_fragment) {
         setupViews()
         arguments?.let { Timber.d(arguments.toString()) }
     }
-
-    /**
-     * 電話アプリを起動
-     *
-     * @param tel
-     */
-    private fun openTell(tel: String) {
-        // アナリティクス イベント送信
-        val bundle = Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "tel")
-        }
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
-
-        if (TextUtils.isEmpty(tel)) {
-            return
-        }
-        val intent = Intent(
-            Intent.ACTION_VIEW,
-            Uri.parse("tel:$tel"),
-        )
-        try {
-            startActivity(intent)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-    /**
-     * 外部ブラウザを起動
-     *
-     * @param url
-     */
-    private fun openBrowser(url: String) {
-        // アナリティクス イベント送信
-        val bundle = Bundle().apply {
-            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "web")
-        }
-        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
-
-        if (TextUtils.isEmpty(url)) {
-            return
-        }
-        CustomTabUtil.start(requireActivity(), url)
-    }
-
-//    override fun onWebClicked(url: String) {
-//        openBrowser(url)
-//    }
-//
-//    override fun onTelClicked(tel: String) {
-//        openTell(tel)
-//    }
 
     companion object {
         /**
