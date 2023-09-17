aa3648228c518755358105973123fa2c2879e31b
ikemura23 <banbara.work@gmail.com>
2023-09-17 09:12:33 +0900

    feat: PortStatusDetailScreen に TimeTableList を追加

--- androidApp/src/main/java/com/yaeyama/linerchecker/ui/portstatusdetail/PortStatusDetailScreen.kt
+++ androidApp/src/main/java/com/yaeyama/linerchecker/ui/portstatusdetail/PortStatusDetailScreen.kt
@@ -1,14 +1,22 @@
 package com.yaeyama.linerchecker.ui.portstatusdetail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yaeyama.linerchecker.ui.portstatusdetail.component.PortMainStatus
+import com.yaeyama.linerchecker.ui.portstatusdetail.component.TimeRow
+import com.yaeyama.linerchecker.ui.portstatusdetail.component.TimeTableList
 import com.yaeyama.linerchecker.ui.theme.YaimafuniAndroidTheme
 import com.yaeyama_liner_checker.domain.statusdetail.Status
+import com.yaeyama_liner_checker.domain.time_table.Header
+import com.yaeyama_liner_checker.domain.time_table.RowItem
+import com.yaeyama_liner_checker.domain.time_table.TimeTable
 
 /**
  * どこからも使われていない？
@@ -18,6 +26,7 @@ fun PortStatusDetailScreen(
     portName: String,
     status: Status,
     statusDescription: String,
+    timeTable: TimeTable,
 ) {
     Card(
         modifier = Modifier
@@ -30,6 +39,12 @@ fun PortStatusDetailScreen(
                 status = status,
                 statusDescription = statusDescription,
             )
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            TimeTableList(
+                timeTable = timeTable,
+            )
         }
     }
 }
@@ -37,11 +52,30 @@ fun PortStatusDetailScreen(
 @Preview
 @Composable
 private fun PortStatusDetailScreenPreview() {
+    val row = TimeRow(
+        left = RowItem(
+            status = Status(code = "nomal", text = "通常運行"),
+            time = "00:00",
+            memo = "",
+        ),
+        right = RowItem(
+            status = Status(code = "cancel", text = "通常運行"),
+            time = "00:00",
+            memo = "",
+        ),
+    )
+    val rows = List(5) { row }
+    val dummyTimeTable = TimeTable(
+        header = Header(left = "石垣島", right = "大原港"),
+        row = rows,
+    )
+
     YaimafuniAndroidTheme {
         PortStatusDetailScreen(
             portName = "portName",
             status = Status(code = "normal", text = "text"),
             statusDescription = "statusDescription",
+            timeTable = dummyTimeTable,
         )
     }
 }
