1c6b861fbf81cfd4f88f9a0409e162f03e62d9d2
Kazutaka Ikemura <banbara.work@gmail.com>
2024-03-14 18:34:10 +0900

    feat: YaimafuniScaffold をDashboardに使う

--- app/src/main/java/com/ikemura23/yaeyama/linerchecker/ui/compose/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/ikemura23/yaeyama/linerchecker/ui/compose/dashboard/DashboardScreen.kt
@@ -1,13 +1,20 @@
 package com.ikemura23.yaeyama.linerchecker.ui.compose.dashboard
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ikemura23.yaeyama.linerchecker.R
+import com.ikemura23.yaeyama.linerchecker.ui.compose.YaimafuniScaffold
 import com.ikemura23.yaeyama.linerchecker.ui.theme.YaimafuniAndroidTheme
 
 @Composable
@@ -15,14 +22,25 @@ fun DashboardScreen(
     modifier: Modifier = Modifier,
     onRowClick: () -> Unit,
 ) {
-
-    Scaffold(
+    YaimafuniScaffold(
         modifier = modifier,
         topBar = {
             DashboardAppBar()
         },
         containerColor = Color.Transparent,
-    ) {contentPadding ->
+    ) { contentPadding ->
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = null,
+                alignment = Alignment.TopEnd,
+                modifier = Modifier.size(200.dp), // 画像サイズ
+                contentScale = ContentScale.Fit // アスペクト比を維持
+
+            )
+        }
         DashboardPage(
             modifier = Modifier.padding(top = contentPadding.calculateTopPadding()),
             onRowClick = onRowClick,
