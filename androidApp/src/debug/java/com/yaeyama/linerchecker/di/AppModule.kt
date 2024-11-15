45a30e51eae52e4c1993767f8388c2fbd30bbb68
Kazutaka Ikemura <banbara.work@gmail.com>
2024-11-15 09:21:51 +0900

    fix: debugでReviewManagerがないエラーが発生するので修正

--- androidApp/src/debug/java/com/yaeyama/linerchecker/di/AppModule.kt
+++ androidApp/src/debug/java/com/yaeyama/linerchecker/di/AppModule.kt
@@ -1,10 +1,11 @@
 package com.yaeyama.linerchecker.di
 
+import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.testing.FakeReviewManager
 import org.koin.dsl.module
 
 val appModule = module {
-    single {
+    single<ReviewManager> {
         FakeReviewManager(get())
     }
 }
