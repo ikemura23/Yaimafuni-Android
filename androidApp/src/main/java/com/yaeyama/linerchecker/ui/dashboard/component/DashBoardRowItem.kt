f3e8f769bfb98ee438fdbf49f09645771e10860d
Kazutaka Ikemura <banbara.work@gmail.com>
2025-10-21 23:59:18 +0900

    feat: DashBoardRowItem を material3 に移行した

--- androidApp/src/main/java/com/yaeyama/linerchecker/ui/dashboard/component/DashBoardRowItem.kt
+++ androidApp/src/main/java/com/yaeyama/linerchecker/ui/dashboard/component/DashBoardRowItem.kt
@@ -6,9 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,13 +35,13 @@ fun DashBoardRowItem(
         if (status.code.isNotEmpty()) {
             Text(
                 text = portName,
-                style = MaterialTheme.typography.body2,
+                style = MaterialTheme.typography.bodyMedium,
             )
             Spacer(modifier = Modifier.padding(4.dp))
             Text(
                 text = status.text,
                 color = Color.White,
-                style = MaterialTheme.typography.body2,
+                style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier
                     .background(
                         color = statusBackgroundColor,
@@ -54,20 +53,18 @@ fun DashBoardRowItem(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun DashBoardRowItemPreview(
     @PreviewParameter(ItemPreviewProvider::class)
     item: DashBoardRowItem,
 ) {
     YaimafuniAndroidTheme {
-        Surface {
-            DashBoardRowItem(
-                portName = item.portName,
-                status = item.status,
-                modifier = Modifier,
-            )
-        }
+        DashBoardRowItem(
+            portName = item.portName,
+            status = item.status,
+            modifier = Modifier,
+        )
     }
 }
 
